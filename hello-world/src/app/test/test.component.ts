8ccda1205671b802f103dc2f46d6dc90bdaa037f
renukumar07 <renukumar90@gmail.com>
2022-03-30 12:39:39 +0530

    inline template in one line

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-test]',
-  templateUrl: './test.component.html',
+  template: '<div>Inline template</div>',
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
