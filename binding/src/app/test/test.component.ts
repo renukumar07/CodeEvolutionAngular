c975c2eccea5a92592dd981ceedec1d94a122450
renukumar07 <renukumar90@gmail.com>
2022-03-31 09:34:35 +0530

    05 Interpolation: Window global object can be accessed in typescript and store in variable and used

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -11,13 +11,14 @@ import { Component, OnInit } from '@angular/core';
     <h2>{{name.length}}</h2>
     <h2>{{name.toUpperCase()}}</h2>
     <h2>{{greetUser()}}</h2>
-    <h2>{{window.location.href}}</h2>
+    <h2>{{siteUrl}}</h2>
   `,
   styleUrls: []
 })
 export class TestComponent implements OnInit {
 
   public name = "Renu"
+  public siteUrl = window.location.href;
   constructor() { }
 
   ngOnInit(): void {
