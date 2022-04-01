f3403251ccdc4f23bffc3c6ca2409b5f2be28eff
renukumar07 <renukumar90@gmail.com>
2022-04-01 12:35:42 +0530

    06 Property Binding: [] for property binding recomended way

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -6,12 +6,14 @@ import { Component, OnInit } from '@angular/core';
     <h2>
       Welcome {{name}}
     </h2>
+    <input [id]="myId" type="text" value="Renu Kumar"/>
   `,
   styleUrls: []
 })
 export class TestComponent implements OnInit {
 
   public name = "Renu"
+  public myId = "testId"
   constructor() { }
 
   ngOnInit(): void {
